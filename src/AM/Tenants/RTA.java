1d450a3146dd5083143987f1ac9220ffa815d8b3
Christopher F. Leo <leofchris@gmail.com>
2019-07-30 14:16:23 -0400

    Init

---
+++ src/AM/Tenants/RTA.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AM.Tenants;
+
+/**
+ *
+ * @author Chris
+ */
+public class RTA {
+   
+    // Tenant First and Last Name
+    // Rental Unit Address
+    // Landlord Name
+    // Number of Vehicles and location
+    // If condominum or not
+    // Agreeable address to send tenants email
+    // Tenants Email address 
+   
+    
+  
+}
